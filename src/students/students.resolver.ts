38e0cfd64312897250e8761566681daf85308335
Arya Rana <aryarana.mscit20@vnsgu.ac.in>
2023-05-05 16:37:21 +0530

    Pushing on master branch

--- src/students/students.resolver.ts
+++ src/students/students.resolver.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { StudentsType } from './students.type';
 import { StudentsService } from './students.service';
